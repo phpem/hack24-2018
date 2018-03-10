54a78e18cfce6aa798f3a955bd450a2277dbb427
Gareth Jones <me@gazj.co.uk>
2018-03-10 17:45:53 +0000

    rats

--- tests/WebhookControllerTest.php
+++ tests/WebhookControllerTest.php
@@ -4,6 +4,7 @@ namespace App\Tests;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Bundle\FrameworkBundle\Client;
+use Symfony\Component\HttpFoundation\Response;
 
 class WebhookControllerTest extends WebTestCase {
 
@@ -20,9 +21,9 @@ class WebhookControllerTest extends WebTestCase {
     public function testWebHookForTransaction()
     {
         $this->client->request('POST', '/webhook/transaction');
+        $response = $this->client->getResponse();
 
-        $response = $this->client->getResponse()->getContent();
-
-        $this->assertEquals('', $response);
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        $this->assertEquals('', $response->getContent());
     }
 }
